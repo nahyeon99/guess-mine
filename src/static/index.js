9fb559ef62f538c54820c7ec256b9032a32815f3
NaHyeon <nahyeonee99@gmail.com>
2021-03-24 22:18:17 +0900

    #2.3 Chat with SocketIO

--- src/static/index.js
+++ src/static/index.js
@@ -4,3 +4,19 @@ const socket = io("/");
 socket.on("hello", () => console.log("Somebody Joined"));
 
 setTimeout(() => socket.emit("helloGuys"), 4000);
+
+function sendMessage(message) {
+  socket.emit("newMessage", { message });
+  console.log(`You: ${message}`);
+}
+
+function setNickname(nickname) {
+  socket.emit("setNickname", { nickname });
+}
+
+function handleMessageNotif(data) {
+  const { message, nickname } = data;
+  console.log(`${nickname}: ${message}`);
+}
+
+socket.on("messageNotif", handleMessageNotif);
